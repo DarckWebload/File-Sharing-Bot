0dcbafc0b3cc188dd5e3dcce791eef557592e4a7
DarckWebload <106021000+DarckWebload@users.noreply.github.com>
2022-06-23 09:55:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("ᴄʟᴏsᴇ ⛔", callback_data = "close")
                 ]
             ]
         )
